15f479bbb5e74716f72f4456bfce6b612414ad41
2026kvsr-blip <2026kvsr@gmail.com>
2026-07-06 23:17:09 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,36 @@
 /* =====================================
 
 oneFaMiLe V1
-Part 1.1C
+Part 1A.3
 
 ===================================== */
 
-/* LOGIN */
+
+/* WELCOME SCREEN */
+
+const welcomePage =
+document.getElementById("welcomePage");
+
+
+/* LOGIN SCREEN */
+
+const loginPage =
+document.getElementById("loginPage");
+
+
+/* OTP SCREEN */
+
+const otpPage =
+document.getElementById("otpPage");
+
+
+/* DASHBOARD */
+
+const dashboard =
+document.getElementById("dashboard");
+
+
+/* BUTTONS */
 
 const loginBtn =
 document.getElementById("loginBtn");
@@ -13,735 +38,113 @@ document.getElementById("loginBtn");
 const signupBtn =
 document.getElementById("signupBtn");
 
-const loginArea =
-document.getElementById("loginArea");
+const sendOTPBtn =
+document.getElementById("sendOTPBtn");
 
-const signupArea =
-document.getElementById("signupArea");
-
-
-
-/* ======================
-
-MENU
-
-====================== */
-
-const menuBtn =
-
-document.querySelector(
-
-".menu-btn"
-
-);
-
-
-const menuPanel =
-
-document.getElementById(
-
-"menuPanel"
-
-);
-
-
-
-menuBtn.addEventListener(
-
-"click",
-
-() => {
-
-menuPanel.classList.toggle(
-
-"hidden"
-
-);
-
-}
-
-);
-
-
-
-/* ======================
-
-EXIT
-
-====================== */
-
-
-const exitBtn =
-
-document.getElementById(
-
-"exitBtn"
-
-);
-
-
-const exitMessage =
-
-document.getElementById(
-
-"exitMessage"
-
-);
-
-
-
-exitBtn.addEventListener(
-
-"click",
-
-() => {
-
-
-menuPanel.classList.add(
-
-"hidden"
-
-);
-
-
-exitMessage.classList.remove(
-
-"hidden"
-
-);
-
-
-
-setTimeout(
-
-() => {
-
-exitMessage.classList.add(
-
-"hidden"
-
-);
-
-},
-
-3000
-
-);
-
-
-}
-
-);
-
-
-
-/* ======================
-
-BOTTOM BUTTONS
-
-
-====================== */
-
-const bottomButtons =
-
-document.querySelectorAll(
-
-".bottom-btn"
-
-);
-
-
-
-bottomButtons.forEach(
-
-(button)=>{
-
-
-button.addEventListener(
-
-"click",
-
-()=>{
-
-
-bottomButtons.forEach(
-
-b =>
-
-b.classList.remove(
-
-"active"
-
-)
-
-);
-
-
-button.classList.add(
-
-"active"
-
-);
-
-
-});
-
-
-});
-
-
-
-/* ======================
-
-TOP BUTTONS
-
-====================== */
-
-
-const moduleButtons =
-
-document.querySelectorAll(
-
-".module-btn"
-
-);
-
-
-
-moduleButtons.forEach(
-
-(button)=>{
-
-
-button.addEventListener(
-
-"click",
-
-()=>{
-
-
-moduleButtons.forEach(
-
-b =>
-
-b.classList.remove(
-
-"active"
-
-)
-
-);
-
-
-button.classList.add(
-
-"active"
-
-);
-
-
-});
-
-
-});
-
-
-
-/* ======================
-
-LANGUAGE
-
-====================== */
-
-
-const lang =
-
-document.getElementById(
-
-"lang"
-
-);
-
-
-
-lang.addEventListener(
-
-"change",
-
-function(){
-
-
-const selected =
-
-this.value;
-
-
-console.log(
-
-"Language : " +
-
-selected
-
-);
-
-
-});
-const homeBtn =
-document.getElementById("homeBtn");
-
-const familyBtn =
-document.getElementById("familyBtn");
-
-const memoriesBtn =
-document.getElementById("memoriesBtn");
-
-const chartsBtn =
-document.getElementById("chartsBtn");
-
-const reportsBtn =
-document.getElementById("reportsBtn");
-
-const homePage =
-document.getElementById("homePage");
-
-const familyPage =
-document.getElementById("familyPage");
-
-const memoriesPage =
-document.getElementById("memoriesPage");
-
-const chartsPage =
-document.getElementById("chartsPage");
-
-const reportsPage =
-document.getElementById("reportsPage");
-const topContainer =
-document.getElementById("topContainer");
-const bottomContainer =
-document.getElementById(
-"bottomContainer"
-);
 const verifyOTPBtn =
-document.getElementById(
-"verifyOTPBtn"
-);
-const welcomeBox =
-document.getElementById(
-"welcomeBox"
-);
-const languageArea =
-document.getElementById("languageArea");
-function hidePages(){
+document.getElementById("verifyOTPBtn");
 
-homePage.classList.add("hidden");
+const backLoginBtn =
+document.getElementById("backLoginBtn");
 
-familyPage.classList.add("hidden");
-
-memoriesPage.classList.add("hidden");
-
-chartsPage.classList.add("hidden");
-
-reportsPage.classList.add("hidden");
-
-}
-function clearBottom(){
-
-bottomButtons.forEach(
-
-b=>{
-
-b.classList.remove(
-
-"active"
-
-);
-
-}
-
-);
-
-}
-topContainer.classList.add(
-"hidden"
-);
-
-bottomContainer.classList.add(
-"hidden"
-);
-homeBtn.onclick = ()=>{
-
-hidePages();
-
-clearBottom();
-
-homeBtn.classList.add(
-
-"active"
-
-);
-
-homePage.classList.remove(
-
-"hidden"
-
-);
-
-topContainer.classList.remove(
-
-"hidden"
-
-);
-
-languageArea.classList.remove(
-
-"hidden"
-
-);
-
-}
-familyBtn.onclick=()=>{
-
-hidePages();
-
-clearBottom();
-
-familyBtn.classList.add(
-
-"active"
-
-);
-
-familyPage.classList.remove(
-
-"hidden"
-
-);
-
-topContainer.classList.add(
-
-"hidden"
-
-);
-
-languageArea.classList.add(
-
-"hidden"
-
-);
-
-}
-memoriesBtn.onclick=()=>{
-
-hidePages();
-
-clearBottom();
-
-memoriesBtn.classList.add(
-
-"active"
-
-);
-
-memoriesPage.classList.remove(
-
-"hidden"
-
-);
-
-topContainer.classList.add(
-
-"hidden"
-
-);
-
-languageArea.classList.add(
-
-"hidden"
-
-);
-
-}
-chartsBtn.onclick=()=>{
-
-hidePages();
-
-clearBottom();
-
-chartsBtn.classList.add(
-
-"active"
-
-);
-
-chartsPage.classList.remove(
-
-"hidden"
-
-);
-
-topContainer.classList.add(
-
-"hidden"
-
-);
-
-languageArea.classList.add(
-
-"hidden"
-
-);
-
-}
-reportsBtn.onclick=()=>{
-
-hidePages();
-
-clearBottom();
-
-reportsBtn.classList.add(
-
-"active"
-
-);
-
-reportsPage.classList.remove(
-
-"hidden"
-
-);
-
-topContainer.classList.add(
-
-"hidden"
-
-);
-
-languageArea.classList.add(
-
-"hidden"
-
-);
-
-}
-
-/* ======================
-
-HOME DEFAULT
-
-====================== */
-
-
-document.querySelector(
-
-".bottom-btn"
-
-)
-
-.classList.add(
-
-"active"
-
-);
-
-
+const backOTPBtn =
+document.getElementById("backOTPBtn");
 
 
 /* ======================
 
-PLACEHOLDER PAGES
+LOGIN
 
 ====================== */
 
-
-/* ======================
-
-TOP BUTTON PLACEHOLDER
-
-====================== */
-
-const homeContent =
-document.getElementById(
-"homeContent"
-);
-
-
-const expensesBtn =
-document.getElementById(
-"expensesBtn"
-);
-
-const activitiesBtn =
-document.getElementById(
-"activitiesBtn"
-);
-
-const loansBtn =
-document.getElementById(
-"loansBtn"
-);
-
-const incomeBtn =
-document.getElementById(
-"incomeBtn"
-);
-
-const healthBtn =
-document.getElementById(
-"healthBtn"
-);
-
-
-
-expensesBtn.onclick=()=>{
-
-
-moduleButtons.forEach(
-
-b=>{
-
-b.classList.remove(
-
-"active"
-
-);
-
-}
-
-);
-
-
-expensesBtn.classList.add(
-
-"active"
-
-);
-
-
-
-homeContent.innerHTML=`
-
-<div class="grid-3x2">
-
-<button class="grid-btn">
-
-Add Expense
-
-</button>
-
-
-<button class="grid-btn">
-
-Reports
-
-</button>
-
-
-<button class="grid-btn">
-
-Sensitive Reports
-
-</button>
-
-
-<button class="grid-btn">
-
-All Reports
-
-</button>
-
-
-<button class="grid-btn">
-
-Custom Search
-
-</button>
-
-
-<button class="grid-btn">
-
-About
-
-</button>
-
-
-</div>
-
-
-<div align="center">
-
-<button
-
-id="backExpense"
-
-class="back-btn">
-
-← Back
-
-</button>
-
-</div>
-
-`;
-
-
-
-document.getElementById(
-
-"backExpense"
-
-)
-
-.onclick=()=>{
-
-
-homeContent.innerHTML=
-
-"<h2>Home</h2>";
-
-
-expensesBtn.classList.remove(
-
-"active"
-
-);
-
-
-};
-
-
-};
-
-
-verifyOTPBtn.onclick = ()=>{
-
-welcomeBox.classList.add("hidden");
-
-loginArea.classList.add("hidden");
-
-signupArea.classList.add("hidden");
-
-topContainer.classList.remove("hidden");
-
-bottomContainer.classList.remove("hidden");
-
-languageArea.classList.remove("hidden");
-
-};
 loginBtn.onclick = ()=>{
 
-loginArea.classList.remove("hidden");
+welcomePage.classList.add("hidden");
 
-signupArea.classList.add("hidden");
+loginPage.classList.remove("hidden");
 
 };
+
+
+
+/* ======================
+
+SIGNUP
+
+====================== */
 
 signupBtn.onclick = ()=>{
 
-signupArea.classList.remove("hidden");
-
-loginArea.classList.add("hidden");
+alert("SignUp Flow Part-1B");
 
 };
+
+
+
+/* ======================
+
+BACK LOGIN
+
+====================== */
+
+backLoginBtn.onclick = ()=>{
+
+loginPage.classList.add("hidden");
+
+welcomePage.classList.remove("hidden");
+
+};
+
+
+
+/* ======================
+
+SEND OTP
+
+====================== */
+
+sendOTPBtn.onclick = ()=>{
+
+loginPage.classList.add("hidden");
+
+otpPage.classList.remove("hidden");
+
+};
+
+
+
+/* ======================
+
+BACK OTP
+
+====================== */
+
+backOTPBtn.onclick = ()=>{
+
+otpPage.classList.add("hidden");
+
+loginPage.classList.remove("hidden");
+
+};
+
+
+
+/* ======================
+
+VERIFY OTP
+
+====================== */
+
+verifyOTPBtn.onclick = ()=>{
+
+otpPage.classList.add("hidden");
+
+dashboard.classList.remove("hidden");
+
+};
+
+
+
 /* ======================
 
 END
